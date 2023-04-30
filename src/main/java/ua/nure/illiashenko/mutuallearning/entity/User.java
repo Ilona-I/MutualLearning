4e717cd1a4d65ca0a8d10e4a637a3980bb3548b5
ilona illiashenko <ilona.illiashenko@gmail.com>
2023-04-30 11:14:05 +0200

    Initial commit

---
+++ src/main/java/ua/nure/illiashenko/mutuallearning/entity/User.java
@@ -0,0 +1,13 @@
+package ua.nure.illiashenko.mutuallearning.entity;
+
+public class User {
+
+    private String login;
+    private String password;
+    private String name;
+    private String email;
+    private String info;
+    private String role;
+    private String level;
+    private String status;
+}
