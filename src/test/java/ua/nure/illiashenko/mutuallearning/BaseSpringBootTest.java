c796e561a42c111388a792738b949ed5e3126bb0
ilona illiashenko <ilona.illiashenko@gmail.com>
2023-06-13 19:35:54 +0200

    Fix tests

--- src/test/java/ua/nure/illiashenko/mutuallearning/BaseSpringBootTest.java
+++ src/test/java/ua/nure/illiashenko/mutuallearning/BaseSpringBootTest.java
@@ -2,7 +2,7 @@ package ua.nure.illiashenko.mutuallearning;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "STRIPE_SECRET_KEY=STRIPE_SECRET_KEY")
+@SpringBootTest(properties = {"STRIPE_SECRET_KEY=STRIPE_SECRET_KEY", "mySqlBinPath=mySqlBinPath"})
 public class BaseSpringBootTest {
 
 }
